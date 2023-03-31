1bd11e2199751572644a627ff6119cca2808ea9c
Sidnei Simeão <sidnei.simeao@vilanova.com.br>
2023-03-31 18:00:26 -0300

    configurações de api no laravel

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryControler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::apiResource('categories', CategoryControler::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', fn () => response()->json(['message' => 'success']));
